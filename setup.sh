867e89481adc7c1584914f05abcba1c90c8047e9
Colin Zabransky <zabransky.colin@gmail.com>
2024-10-19 18:55:48 -0400

    symlink home-manager

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ if [ -z $USERNAME ]; then
 fi
 echo $USERNAME
 
+
+# Symlink home-manger to the config directory
+ln -s ~/dotfiles/home-manager ~/.config/home-manager
+
 # Set the default shell to fish
 sudo sed -i 's/required/sufficient/g' /etc/pam.d/chsh
 sudo sh -c 'printf "%s\n" /home/'"$USERNAME"'/.nix-profile/bin/fish >> /etc/shells'
