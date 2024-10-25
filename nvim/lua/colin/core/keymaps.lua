e4712382624d6f2c9516f9480ff2249f0b99c9e0
czabransky <zabransky.colin@gmail.com>
2024-10-25 14:32:34 -0400

    add comment on vs behaviour

--- nvim/lua/colin/core/keymaps.lua
+++ nvim/lua/colin/core/keymaps.lua
@@ -61,5 +61,6 @@ end
 
 -- VS Code and Visual Studio Maps
 -- For these keymaps to function, you need VS Code and/or Visual Studio executables to exist in the $PATH
+-- As of 10/25/2024, you cannot combine /edit with /command, e.g., `devenv /edit % /command "GotoLn 50"`
 vim.keymap.set("n", "<leader>gc", ":!code %<CR>", { desc = "Open Current File in Visual Studio Code" })
 vim.keymap.set("n", "<leader>gs", ":!devenv /edit %<CR>", { desc = "Open Current File in Visual Studio" })
