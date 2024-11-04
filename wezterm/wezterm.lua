76b751a58df0b961554227e736ffa28d307d4898
czabransky <zabransky.colin@gmail.com>
2024-11-03 19:02:13 -0500

    set wezterm keybinds for splits that follow neovim

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,5 +4,26 @@ return {
 	default_prog = { "pwsh.exe", "-NoLogo" },
 	enable_tab_bar = false,
 	font = wezterm.font({ family = "RobotoMono Nerd Font" }),
-	-- window_decorations = "NONE | RESIZE",
+	window_decorations = "NONE | RESIZE",
+
+	-- follow tmux/nvim keybinds
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		{
+			key = "|",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "-",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+		{
+			key = "a",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+		},
+	},
 }
