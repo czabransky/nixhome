5cb3bf2bcd5a091715ee8d2ecd32ebb25050f1e8
czabransky <zabransky.colin@gmail.com>
2025-01-12 12:51:40 -0500

    disable snacks.nvim scroll module

---
+++ lazyvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
