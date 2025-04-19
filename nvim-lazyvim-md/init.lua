cd78ffe51b2340a654aae6e1371ec736a5dbdde7
czabransky <zabransky.colin@gmail.com>
2025-04-19 13:50:02 -0400

    updated md options

--- nvim-lazyvim-md/init.lua
+++ nvim-lazyvim-md/init.lua
@@ -2,3 +2,13 @@
 require("config.lazy")
 
 vim.cmd.colorscheme("catppuccin-frappe")
+
+-- Options to make markdown more readable
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↪ "
+vim.opt.colorcolumn = "80"
+
+vim.opt.spell = true
+vim.opt.textwidth = 80 -- this affects `gq` formatting, not visual wrap
