ac22eed620ceb83e0b36bae46ac5fa2da07e8056
czabransky <zabransky.colin@gmail.com>
2025-05-09 16:20:05 -0400

    update vimrc

--- nvim-lazyvim/lua/plugins/disabled.lua
+++ nvim-lazyvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "aechasnovski/mini.pairs", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
 }
