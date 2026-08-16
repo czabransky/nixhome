55cfe27e4d7ff25aaaf93e7ffdc76be2d373ebe0
Colin Zabransky <zabransky.colin@gmail.com>
2026-08-16 08:03:59 -0400

    lofi updated url

--- fish/functions/lofi.fish
+++ fish/functions/lofi.fish
@@ -1,4 +1,4 @@
 function lofi --description "opens lofi beats in the default browser"
-	open "https://www.youtube.com/watch?v=jfKfPfyJRdk"
+	open "https://www.youtube.com/watch?v=X4VbdwhkE10"
 end
 
