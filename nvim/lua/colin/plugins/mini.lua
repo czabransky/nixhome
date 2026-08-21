afd1a5c205f5f8e0c8c9880d80bb70e3a0877e5b
Colin Zabransky <zabransky.colin@gmail.com>
2026-08-21 15:50:26 -0400

    nvim - animations

--- nvim/lua/colin/plugins/mini.lua
+++ nvim/lua/colin/plugins/mini.lua
@@ -2,7 +2,12 @@ return {
 	"echasnovski/mini.nvim",
 	version = "*",
 	config = function()
-		require("mini.animate").setup({
+		local animate = require("mini.animate")
+		animate.setup({
+			-- Cursor/scroll animation is screen-share/pairing candy (lets
+			-- viewers visually track jumps and scrolling) but distracting for
+			-- solo work, so both start off and are toggleable below. resize
+			-- always stays on - it's not a common-enough action to need one.
 			cursor = {
 				enable = false,
 				timing = function(_, n)
@@ -22,6 +27,14 @@ return {
 				end,
 			},
 		})
+
+		vim.keymap.set("n", "<leader>cu", function()
+			animate.config.cursor.enable = not animate.config.cursor.enable
+		end, { desc = "[C]ode Toggle C[u]rsor Animation" })
+
+		vim.keymap.set("n", "<leader>cs", function()
+			animate.config.scroll.enable = not animate.config.scroll.enable
+		end, { desc = "[C]ode Toggle [S]croll Animation" })
 		require("mini.comment").setup()
 		require("mini.pairs").setup()
 		local hipatterns = require("mini.hipatterns")
