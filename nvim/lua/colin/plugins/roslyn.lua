00c1be79343ce407512ef19d78033343d0feb7aa
Colin Zabransky <zabransky.colin@gmail.com>
2026-08-23 10:57:06 -0400

    nvim - attach rosyln

--- nvim/lua/colin/plugins/roslyn.lua
+++ nvim/lua/colin/plugins/roslyn.lua
@@ -1,10 +1,18 @@
 return {
-    "seblyng/roslyn.nvim",
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {
-		ignore_target = function(target)
-			return string.match(target, "Unified.sln") ~= nil
-		end
-    },
+	"seblyng/roslyn.nvim",
+	config = function()
+		local setup = require("colin.lsp.lsp-setup")
+		local attach = require("colin.lsp.lsp-attach")
+
+		-- roslyn.nvim's own setup() only takes target-resolution/filewatching
+		-- options (RoslynNvimConfig) - on_attach/capabilities go through the
+		-- native vim.lsp.config() API instead, same as the other lsp/config/*
+		-- servers, and merge into roslyn.nvim's bundled lsp/roslyn.lua config.
+		vim.lsp.config("roslyn", {
+			capabilities = setup.capabilities_with_snippets,
+			on_attach = attach.on_attach,
+		})
+
+		require("roslyn").setup({})
+	end,
 }
