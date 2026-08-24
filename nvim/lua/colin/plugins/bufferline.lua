593e1f5e663c1c59ec10b73feb63e82acfd94fec
Colin Zabransky <zabransky.colin@gmail.com>
2026-08-24 11:49:35 -0400

    nvim - bufferline filters out qflist

--- nvim/lua/colin/plugins/bufferline.lua
+++ nvim/lua/colin/plugins/bufferline.lua
@@ -8,6 +8,11 @@ return {
 			offsets = {
 				{ filetype = "NvimTree", text = "Explorer", separator = true },
 			},
+			-- Quickfix/location-list windows are unnamed ("[No Name]") and
+			-- share buftype="quickfix" - hide them from the tab bar.
+			custom_filter = function(buf_number)
+				return vim.bo[buf_number].buftype ~= "quickfix"
+			end,
 		},
 	},
 }
