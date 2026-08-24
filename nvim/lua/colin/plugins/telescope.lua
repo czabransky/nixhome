422440abf7e0e0a81bd33d0f93cad9793d45740e
Colin Zabransky <zabransky.colin@gmail.com>
2026-08-24 10:30:07 -0400

    nvim - telescope keybind updates

--- nvim/lua/colin/plugins/telescope.lua
+++ nvim/lua/colin/plugins/telescope.lua
@@ -114,17 +114,17 @@ return {
 
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
 		vim.keymap.set("n", "<leader>sF", find_all_files, { desc = "Search All Files" })
-		vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "Search Commands" })
+		vim.keymap.set("n", "<leader>sC", builtin.commands, { desc = "Search Commands" })
 		vim.keymap.set("n", "<leader>s/", live_grep_open_files, { desc = "Search / in Open Files" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search Files With Grep" })
 		vim.keymap.set("n", "<leader>sG", builtin.git_files, { desc = "Search Git Files" })
-		vim.keymap.set("n", "<leader>sS", builtin.git_status, { desc = "Search Git Status (Changed Files)" })
+		vim.keymap.set("n", "<leader>sc", builtin.git_status, { desc = "Search Git Status (Changed Files)" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Search Current Word" })
 		vim.keymap.set("n", "<leader>sr", builtin.oldfiles, { desc = "Search Recent Files" })
 		vim.keymap.set("n", "<leader>sp", builtin.resume, { desc = "Search Previous (Resume State)" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
 		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Search Open Buffers" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
+		vim.keymap.set("n", "<leader>sx", builtin.diagnostics, { desc = "Search Diagnostics" })
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
 	end,
 }
